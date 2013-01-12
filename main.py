9c1d7c67ca992cf7107e1737bde8805a78a180fd
Donald Hui[Windows] <donaldhui@groupthinq.com>
2013-01-11 22:43:10 -0800

    finished add card funcitonality, starting on search

--- main.py
+++ main.py
@@ -19,15 +19,45 @@ class CreateCardHandler(tornado.web.RequestHandler):
         print "post request"
         cname = self.get_argument('name')
         cdesc = self.get_argument('desc', default="")
-        costs = self.get_argument('cost[]')
+        costs = self.request.arguments.get('cost[]')
         img_url = self.get_argument('img_url', default="")
         flavor_text = self.get_argument('flavor_text', default="")
+        
+        total_cost =[] 
+        cost_dict = {}
+        for c in costs:
+            if type(c) == int:
+                cost_dict['colorless'] = cost_dict.get(c, 0) + 1
+            else:
+                cost_dict[str(c)] = cost_dict.get(str(c), 0) + 1
+
+        colors = ['b','g','r','w','u','colorless']
+
+        for color in colors:
+            if color not in cost_dict:
+                cost_dict[color] = 0
+
+        for key, val in cost_dict.items():
+            total_cost.append(val)
+        
         c = Card.objects(name=cname);
+
         if len(c) > 0:
             self.write("%s already exists" %cname)
             self.flush()
             return
-        c = Card(name=cname, desc=cdesc, total_cost=[costs], ccost=1, image_path=img_url, flv=flavor_text)
+        c = Card(name=cname, 
+                desc=cdesc, 
+                total_cost=total_cost,
+                ccost=cost_dict['colorless'],
+                bcost=cost_dict['b'], 
+                gcost=cost_dict['g'],
+                rcost=cost_dict['r'],
+                wcost=cost_dict['w'],
+                ucost=cost_dict['u'],
+                image_path=img_url,
+                flv=flavor_text)
+
         c.save()
         print "Add Card was successful"
         print self.request.arguments
